eee0af058b7f3110329c44e6f0f4a072c1238b40
Johnbosco <nwekejohnbosco@gmail.com>
2024-02-01 15:27:26 +0100

    (index.php):register plugin

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Custom API woocommerce Data
+ *
+ * Plugin Name: Custom API woocommerce Data
+ * Plugin URI:  https://github.com/jbchinonso/custom-api-woocommerce-data
+ * Description: Enable Custom API in woocommerce my-account page and as a widget
+ * Version:     1.0
+ * Author:      johnbosco
+ * Author URI:  https://github.com/jbchinonso
+ * License:     MIT
+ * Text Domain: Saucal
+ * Requires PHP at least: 7.0
+ *
+ */
+
+ 
